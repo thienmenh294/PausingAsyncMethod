ca90e3f5530f19012c2cae92dbd73f6953fcb02b
thienmenh294 <nnt294@gmail.com>
2021-08-22 14:22:12 +0900

    Add project files.

---
+++ PausingAsyncMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PausingAsyncMethod
+{
+    class Program
+    {
+        static void Main()
+        {
+            var pts = new PauseTokenSource();
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Console.ReadLine();
+                    pts.IsPaused = !pts.IsPaused;
+                }
+            });
+            SomeMethodAsync(pts.Token).Wait();
+        }
+
+        public static async Task SomeMethodAsync(PauseToken pause)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                await Task.Delay(100);
+                await pause.WaitWhilePausedAsync();
+            }
+        }
+    }
+}
